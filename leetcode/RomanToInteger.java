757c35824c43db7549103f269ed6549e3fa2ee33
Tonghann Teng <tengtonghann@gmail.com>
2020-03-07 14:44:46 -0600

    13.  Roman To Integer

---
+++ leetcode/RomanToInteger.java
@@ -0,0 +1,25 @@
+//  13.  Roman To Integer
+
+class RomanToInteger {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int result = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i + 2 <= s.length()) && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+                result -= map.get(s.charAt(i));
+            } else {
+                result += map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
