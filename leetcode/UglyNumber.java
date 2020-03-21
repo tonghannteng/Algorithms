23ae1e7830a41de9259d2b9cab40917b99f3b804
Tonghann Teng <tengtonghann@gmail.com>
2020-03-20 22:01:51 -0500

    263. ugly number

---
+++ leetcode/UglyNumber.java
@@ -0,0 +1,27 @@
+//  263. ugly number
+
+class UglyNumber {
+
+    public boolean isUgly(int num) {
+        if (num == 0) return false;
+        while (true) {
+            if (num % 2 == 0) {
+                num /= 2;
+                continue;
+            }
+            if (num % 3 == 0) {
+                num /= 3;
+                continue;
+            }
+            if (num % 5 == 0) {
+                num /= 5;
+                continue;
+            }
+            if (num == 1) {
+                return true;
+            } else if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0) {
+                return false;
+            }
+        }
+    }
+}
