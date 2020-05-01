0a5eda1c0ed9ee73b46150f57f5b80a57f7ca05c
Tonghann Teng <tengtonghann@gmail.com>
2020-04-30 21:33:15 -0500

    326. Power of Three

---
+++ leetcode/PowerThree.java
@@ -0,0 +1,11 @@
+//  326. Power of Three
+
+class XKindDeckCards {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
