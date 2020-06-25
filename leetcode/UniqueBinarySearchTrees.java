86178fcd4e8ff63a4263f0b46ce7839a01c46e4e
Tonghann Teng <tengtonghann@gmail.com>
2020-06-24 21:27:44 -0500

    96. Unique Binary Search Trees

---
+++ leetcode/UniqueBinarySearchTrees.java
@@ -0,0 +1,15 @@
+//  96. Unique Binary Search Trees
+
+class UniqueBinarySearchTrees {
+
+    public int numTrees(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            count += numTrees(i - 1) * numTrees(n - i);
+        }
+        return count;
+    }
+}
