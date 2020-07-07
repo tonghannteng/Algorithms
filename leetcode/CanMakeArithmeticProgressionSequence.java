c073f303a9b8e7a2e8b09b541d3b5a1c75409837
Tonghann Teng <tengtonghann@gmail.com>
2020-07-06 21:57:25 -0500

    1502. Can Make Arithmetic Progression From Sequence

---
+++ leetcode/CanMakeArithmeticProgressionSequence.java
@@ -0,0 +1,15 @@
+//  1502. Can Make Arithmetic Progression From Sequence
+
+class CanMakeArithmeticProgressionSequence {
+
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int d = arr[0] - arr[1];
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] - arr[i+1] != d) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
