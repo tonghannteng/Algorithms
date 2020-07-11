3fa0c5b08a24a50e65c64cd99da25d6b5004406f
Tonghann Teng <tengtonghann@gmail.com>
2020-07-11 18:27:27 -0500

    78. Subsets

---
+++ leetcode/Subsets.java
@@ -0,0 +1,21 @@
+//  78. Subsets
+
+class Subsets {
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList();
+        result.add(new ArrayList());
+        for(int num : nums) {
+            int size = result.size();
+            for(int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList(result.get(i));
+                subset.add(num);
+                result.add(subset);
+            }
+        }
+
+        return result;
+    }
+}
+
