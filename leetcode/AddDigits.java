bc8b9acc9944db66d600619384f384f73f3a260c
Tonghann Teng <tengtonghann@gmail.com>
2020-07-26 23:37:17 -0500

    258. Add Digits

---
+++ leetcode/AddDigits.java
@@ -0,0 +1,16 @@
+//  258. Add Digits
+
+class AddDigits {
+
+    public int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        } else {
+            if (num%9 ==0) {
+                return 9;
+            } else {
+                return num%9;
+            }
+        }
+    }
+}
