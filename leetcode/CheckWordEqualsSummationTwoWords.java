47e79a1311729cb896e3e894242a21fee67e979e
Tonghann Teng <tengtonghann@gmail.com>
2021-06-19 00:28:55 -0500

    1880. Check if Word Equals Summation of Two Words

--- leetcode/CheckWordEqualsSummationTwoWords.java
+++ leetcode/CheckWordEqualsSummationTwoWords.java
@@ -3,19 +3,16 @@
 class CheckWordEqualsSummationTwoWords {
 
     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-        StringBuilder f = new StringBuilder();
-        StringBuilder s = new StringBuilder();
-        StringBuilder t = new StringBuilder();
-        for (int i = 0; i < firstWord.length(); i++) {
-            f.append((firstWord.charAt(i) - 'a'));
-        }
-        for (int i = 0; i < secondWord.length(); i++) {
-            s.append(secondWord.charAt(i) - 'a');
-        }
-        for (int i = 0; i < targetWord.length(); i++) {
-            t.append(targetWord.charAt(i) - 'a');
-        }
-        return Integer.parseInt(f.toString()) + Integer.parseInt(s.toString()) == Integer.parseInt(t.toString());
+        return convertToInt(firstWord) + convertToInt(secondWord) == convertToInt(targetWord);
     }
-}
+
+    public int convertToInt(String num) {
+        int result = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            result = result*10 + (num.charAt(i) - 'a');
+
+        }
+        return result;
+    }
 }
