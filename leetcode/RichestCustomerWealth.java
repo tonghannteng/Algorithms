f3c39cdb09a624a20db69463013d8392acb2dc98
Tonghann Teng <tengtonghann@gmail.com>
2021-07-15 23:31:46 -0500

    1672. Richest Customer Wealth

---
+++ leetcode/RichestCustomerWealth.java
@@ -0,0 +1,16 @@
+//  1672. Richest Customer Wealth
+
+class RichestCustomerWealth {
+
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
