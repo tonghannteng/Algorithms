9951e9bd1806e06695c61d75026831c5ca6d8700
Tonghann Teng <tengtonghann@gmail.com>
2022-03-29 00:27:36 -0500

    300. Longest Increasing Subsequence

---
+++ leetcode/LongestIncreasingSubsequence.java
@@ -0,0 +1,24 @@
+//  300. Longest Increasing Subsequence
+
+class LongestIncreasingSubsequence {
+    
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+
+            }
+        }
+
+        int result = 1;
+        for(int num: dp) {
+            result = Math.max(num, result);
+        }
+        return result;
+    }
+}
